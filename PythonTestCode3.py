09efba910c6775f4f9a269f32bfbda3ced08e567
Rina kumari <Rina@gmail.com>
2025-02-12 10:33:00 +0530

    File handling exercise is completd

--- PythonTestCode3.py
+++ PythonTestCode3.py
@@ -67,65 +67,82 @@
 
 
 
-class Operation:
-    # create file & insert record
-    def create_file(self):
-        with open('operation.py','w') as f:
-            pass
+# class Operation:
+#     # create file & insert record
+#     def create_file(self):
+#         with open('operation.py','w') as f:
+#             pass
 
-    def add_text(self,data):
-        with open('operation.py','a') as file:
-            file.write(data + "\n")
+#     def add_text(self,data):
+#         with open('operation.py','a') as file:
+#             file.write(data + "\n")
 
-        with open('operation.py','r') as f:
-            global res 
-            res=f.read()
-            print("\n\n","show all character present in file")
-            print(res)
-            print("\n\nshow the type of data ")
-            print(type(res))
+#         with open('operation.py','r') as f:
+#             global res 
+#             res=f.read()
+#             print("\n\n","show all character present in file")
+#             print(res)
+#             print("\n\nshow the type of data ")
+#             print(type(res))
     
 
 
-    #Here is problem is going on ------------------------------------------------>
-    #show how many char in data in file
-    def count_char_file(self):
-        with open('C:\\Users\\kumar\\Desktop\\Work Space\\WorkSpace\\PythonTestCode3.py','r') as f:
-            res=f.read()
-        print(len(res))
+#     #Here is problem is going on ------------------------------------------------>
+#     #show how many char in data in file
+#     def count_char_file(self):
+#         with open('C:\\Users\\kumar\\Desktop\\Work Space\\WorkSpace\\PythonTestCode3.py','r') as f:
+#             res=f.read()
+#         print(len(res))
 
-    def seperate_val(self):
-        uchar=""
-        lchar=""
-        num=""
-        sp=""
-        c1,c2,c3,c4=0,0,0,0
-        with open("C:\\Users\\kumar\\Desktop\\Work Space\\WorkSpace\\PythonTestCode3.py",'r') as file:
-            res=file.read()
+#     def seperate_val(self):
+#         uchar=""
+#         lchar=""
+#         num=""
+#         sp=""
+#         c1,c2,c3,c4=0,0,0,0
+#         with open("C:\\Users\\kumar\\Desktop\\Work Space\\WorkSpace\\PythonTestCode3.py",'r') as file:
+#             res=file.read()
 
-            for val in res:
-                if val.isupper():
-                    c1=c1+1
-                    uchar=uchar+val 
-                elif val.islower():
-                    c2=c2+1
-                    lchar=lchar+val 
-                elif val.isnumeric():
-                    c3=c3+1
-                    num=num+val
-                elif val.isspace():
-                    c4=c4+1
-                    sp=sp+val 
-        print("\nShow total number of upper letter:",c1,"\nTotal upper clase latter: \n",uchar,)
-        print("\nTowal number of lower letter: ",c2,"\nlower case character: \n",lchar)
-        print("\nTotal number of number : ",c3,"\nTotal number: \n",num)
-        print("\nTotal number of space: \n",c4)
+#             for val in res:
+#                 if val.isupper():
+#                     c1=c1+1
+#                     uchar=uchar+val 
+#                 elif val.islower():
+#                     c2=c2+1
+#                     lchar=lchar+val 
+#                 elif val.isnumeric():
+#                     c3=c3+1
+#                     num=num+val
+#                 elif val.isspace():
+#                     c4=c4+1
+#                     sp=sp+val 
+#         print("\nShow total number of upper letter:",c1,"\nTotal upper clase latter: \n",uchar,)
+#         print("\nTowal number of lower letter: ",c2,"\nlower case character: \n",lchar)
+#         print("\nTotal number of number : ",c3,"\nTotal number: \n",num)
+#         print("\nTotal number of space: \n",c4)
         
-ops=Operation()
-#ops.create_file()
+# ops=Operation()
+# #ops.create_file()
 
-# data=input("Enter text data: ")
-#ops.add_text(data)
+# # data=input("Enter text data: ")
+# #ops.add_text(data)
 
-#ops.count_char_file()
-ops.seperate_val()           
+# #ops.count_char_file()
+# ops.seperate_val()  
+
+
+# #Phase2
+# class Phase3:
+#     def readfile(self,name,n):
+#           f=open(name,'r')
+#           l=f.readlines()
+#           count=0
+#           for a in l:
+#               count=count+1
+#               if n==count:
+#                   print(a)
+
+#           f.close()
+# p=Phase3()
+# name="C:\\Users\\kumar\\Desktop\\Work Space\\WorkSpace\\PythonTestCode3.py"
+# p.readfile(name,5)
